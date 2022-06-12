370ce8f0c0d029fefbeb2155047472591a3d4ca0
April White <april@opsnlops.io>
2022-06-12 00:03:34 -0700

    Constants for our initial BLE service

---
+++ src/joystick-ble/joystick-ble.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+
+#define SERVICE_UUID "2301b64e-e54e-11ec-be04-1315027ce61a"
+#define JOYSTICK0_CHARACTERISTIC_UUID "2eea192e-e54e-11ec-b9ab-f7d98be65acb"
+#define JOYSTICK1_CHARACTERISTIC_UUID "34437dd4-e54e-11ec-8d9e-c723f98ddce5"
+
