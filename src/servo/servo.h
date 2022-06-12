1e839cf4d5f49380a1c2d07ef9d706e00237029e
April White <april@opsnlops.io>
2022-06-12 00:03:14 -0700

    Tweaks to the ranges

--- src/servo/servo.h
+++ src/servo/servo.h
@@ -15,7 +15,7 @@
 #include "logging/logging.h"
 
 // Taken from the Adafruit docs. These might need to be adjusted
-#define SERVOMIN 110  // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMIN 140  // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX 800 // This is the 'maximum' pulse length count (out of 4096)
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
