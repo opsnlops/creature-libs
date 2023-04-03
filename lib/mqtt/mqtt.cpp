e0044d9a45157b33a483c4c664231c8bee26c373
April White <april@opsnlops.io>
2023-04-03 16:52:01 -0700

    Testing SSH signing

---
+++ lib/mqtt/mqtt.cpp
@@ -0,0 +1,73 @@
+
+
+#include <AsyncMqttClient.h>
+#include <WiFi.h>
+
+#include "connection.h"
+#include "mqtt.h"
+
+using namespace std;
+
+void MQTT::configure(IPAddress _mqtt_broker_address, uint16_t _mqtt_broker_port)
+{
+    mqtt_broker_address = _mqtt_broker_address;
+    mqtt_broker_port = _mqtt_broker_port;
+
+    ESP_LOGI(TAG, "The IP of the broker is %s, port: %d", mqtt_broker_address.toString(), mqtt_broker_port);
+}
+
+void MQTT::connectToMqtt()
+{
+
+    mqttClient.setServer(mqtt_broker_address, mqtt_broker_port);
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.onSubscribe(onMqttSubscribe);
+    mqttClient.onUnsubscribe(onMqttUnsubscribe);
+    mqttClient.onPublish(onMqttPublish);
+
+    ESP_LOGI(TAG, "Connecting to MQTT...");
+    mqttClient.connect();
+    ESP_LOGI(TAG, "connected!");
+}
+
+void MQTT::onMqttConnect(bool sessionPresent)
+{
+    ESP_LOGI(TAG, "Connected to MQTT.");
+    ESP_LOGV(TAG, "Session present: %b", sessionPresent);
+
+    // Listen for the magic heartbeat
+    mqttClient.subscribe(HEARTBEAT_TOPIC, 0);
+}
+
+void MQTT::onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
+{
+    ESP_LOGI(TAG, "Disconnected from MQTT.");
+
+    if (WiFi.isConnected())
+    {
+        xTimerStart(mqttReconnectTimer, 0);
+    }
+}
+
+void MQTT::onMqttSubscribe(uint16_t packetId, uint8_t qos)
+{
+    ESP_LOGD(TAG, "Subscribe acknowledged.");
+    ESP_LOGV(TAG, "  packetId: %d, qos: %d", packetId, qos);
+}
+
+void MQTT::onMqttUnsubscribe(uint16_t packetId)
+{
+    ESP_LOGI(TAG, "Unsubscribe acknowledged.");
+    ESP_LOGV(TAG, "  packetId: %d", packetId);
+}
+
+void MQTT::onMqttPublish(uint16_t packetId)
+{
+    ESP_LOGI(TAG, "Publish acknowledged.");
+    ESP_LOGV(TAG, " packetId: %d", packetId);
+}
+
+void MQTT::onWifiDisconnect()
+{
+}
---
+++ lib/mqtt/mqtt.h
@@ -0,0 +1,43 @@
+
+#ifndef _APRILS_CREATURES_MQTT
+#define _APRILS_CREATURES_MQTT
+
+extern "C"
+{
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+
+#define HEARTBEAT_TOPIC "heartbeat"
+
+#define MQTT_ON "on"
+#define MQTT_OFF "off"
+
+
+class MQTT
+{
+    static const char *broker_role;
+    static const char *broker_service;
+    static const char *broker_protocol;
+
+    static IPAddress mqtt_broker_address;
+    static uint16_t mqtt_broker_port;
+
+public:
+
+    static TimerHandle_t mqttReconnectTimer;
+    static AsyncMqttClient mqttClient;
+
+    static void configure(IPAddress mqtt_broker_address, uint16_t mqtt_broker_port);
+    static void connectToMqtt();
+    static void onMqttConnect(bool sessionPresent);
+    static void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+    static void onMqttSubscribe(uint16_t packetId, uint8_t qos);
+    static void onMqttUnsubscribe(uint16_t packetId);
+    static void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+    static void onMqttPublish(uint16_t packetId);
+    static void onWifiDisconnect();
+};
+
+#endif
